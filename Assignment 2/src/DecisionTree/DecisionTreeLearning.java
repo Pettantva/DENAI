dad12263c3e0a9bbff7b6b5cd42dfeb09018795a
pett <35666826+Pettantva@users.noreply.github.com>
2018-02-22 15:40:20 +0100

    ed

---
+++ Assignment 2/src/DecisionTree/DecisionTreeLearning.java
@@ -0,0 +1,33 @@
+package DecisionTree;
+import java.util.*;
+
+public class DecisionTreeLearning {
+	private int[] examples;
+	private String attributes;
+	private int[] parentExamples;
+
+	//Typerna av attributerna ska ändras boi
+	public DecisionTreeLearning(int[] examples, String attributes, int [] parentExamples){
+		this.examples = examples;
+		this.attributes = attributes;
+		this.parentExamples = parentExamples;
+	}
+	//vad som returneras ska ändras boi
+	public String DecisionTree(){
+		if(examples.length == 0){
+			
+		}
+		return "hej";
+	}
+	
+	private String PluralityValue(int[] parentExamples){
+		
+		ArrayList data = new ArrayList<ArrayList>();
+		data
+		return "Lol boi lolz";
+	}
+	
+	private Importance(int i, int j){
+		
+	}
+}
